56663375caf178e98ff17b72a11d644ca804f66f
jjgancfer <jjgancfer@gmail.com>
2023-03-03 23:30:28 +0100

    Added the 'Map' component

---
+++ webapp/src/components/Map.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Map (){
+    return (
+        // This block is intended to be left empty. The map goes here!!
+        <div id = "map">
+
+        </div>
+    );
+}
