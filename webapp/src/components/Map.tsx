1e271c3c7207c357ff01633bf36d5dae7ae027f6
jjgancfer <jjgancfer@gmail.com>
2023-03-03 23:54:59 +0100

    Improved the implementation according to the guides on the API's web

--- webapp/src/components/Map.tsx
+++ webapp/src/components/Map.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import mapboxgl from '../node_modules/mapbox-gl/build'; // eslint-disable-line import/no-webpack-loader-syntax
 
-export function Map (){
+mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+
+export default function Map (){
+
+    // Refer to the documentation of the API:
+    //https://docs.mapbox.com/help/tutorials/use-mapbox-gl-js-with-react/?q=Map&size=n_10_n
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
+
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
     return (
-        // This block is intended to be left empty. The map goes here!!
-        <div id = "map">
-
+        <div>
+            <div ref={mapContainer} className="map-container" />
         </div>
     );
 }
