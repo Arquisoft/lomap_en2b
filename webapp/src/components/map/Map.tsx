4dc21d250edf9947b1bfb85a14dcdd069b17204e
jjgancfer <jjgancfer@gmail.com>
2023-03-06 20:05:23 +0100

    Updated the CSS stylesheet

--- webapp/src/components/map/Map.tsx
+++ webapp/src/components/map/Map.tsx
@@ -1,14 +1,12 @@
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet-css'
 import './Map.css';
 
 export default function Map() {
-    return <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-    </Marker>
-  </MapContainer>
+    return <MapContainer center={[50.847, 4.357]} zoom={12} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
 }
