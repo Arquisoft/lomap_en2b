d9588e3988c4e8b5df13cf80c4049da3c2b2bcb6
jjgancfer <jjgancfer@gmail.com>
2023-03-08 20:49:51 +0100

    Update the implementation to include the new 'Landmark' type

--- webapp/src/components/map/MapMarker.tsx
+++ webapp/src/components/map/MapMarker.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import {Marker, Popup} from "react-leaflet"
+import { Landmark } from "../../shared/shareddtypes";
 
 export default class MapMarker extends React.Component {
 
-    private position : [number, number] = [0,0];
-    private text : string;
+    private mark : Landmark;
 
-    constructor(props : any, coordinates : [number, number], text : string) {
+    constructor(props : any, mark : Landmark) {
         super(props);
-        this.position = coordinates;
-        this.text = text;
+        this.mark = mark;
     }
     
     render() {
-        <Marker position = {this.position}>
+        <Marker position = {[this.mark.longitude, this.mark.latitude]}>
             <Popup>
-                {this.text}
+                {this.mark.name}
+                <br />
+                {this.mark.category}
             </Popup>
         </Marker>
     }
