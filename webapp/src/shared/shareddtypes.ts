da98017146831a7075eb893eea0466d66030e453
plg22 <uo282763@uniovi.es>
2023-03-08 17:09:50 +0100

    First types created

--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -1,4 +1,25 @@
 export type User = {
     name:string;
     email:string;
-  }
+    friends:User[];
+  };
+
+export type Landmark = {
+    name:string;
+    category:string;
+    latitude:number;
+    longitude:number;
+    score?:number;
+    comment?:string;
+    picture?:string;
+};
+
+export const LandmarkCategories = {
+    Shop: "Shop",
+    Bar: "Bar",
+    Restaurant: "Restaurant",
+    Sight: "Sight",
+    Monument: "Monument",
+    Entertainment: "Entertainment",
+    Other: "Other",
+};
