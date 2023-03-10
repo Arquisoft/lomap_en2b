b45c06895b3b376ca3b4b924967e4868cd3e98c9
jjgancfer <jjgancfer@gmail.com>
2023-03-10 20:26:58 +0100

    Refactored the code to include the MapMarker class in the Map file

--- webapp/src/components/map/Map.tsx
+++ webapp/src/components/map/Map.tsx
@@ -1,6 +1,27 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React from 'react';
+import { Landmark } from '../../shared/shareddtypes';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import 'leaflet-css'
 import './Map.css';
+
+class MapMarker extends React.Component {
+
+    private mark : Landmark;
+
+    constructor(props : any, mark : Landmark) {
+        super(props);
+        this.mark = mark;
+    }
+    
+    render() {
+        <Marker position = {[this.mark.longitude, this.mark.latitude]}>
+            <Popup>
+                {this.mark.name} - 
+                {this.mark.category}
+            </Popup>
+        </Marker>
+    }
+}
 
 export default function Map() {
     return <MapContainer center={[50.847, 4.357]} zoom={12} scrollWheelZoom={true}>
@@ -8,5 +29,5 @@ export default function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-        </MapContainer>
+            </MapContainer>
 }
