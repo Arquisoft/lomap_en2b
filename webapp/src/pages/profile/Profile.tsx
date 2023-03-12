4676dc5f71ed2be397c37f6885696d8e3805d953
Andrés Cadenas <andrescabla@gmail.com>
2023-03-12 22:29:33 +0100

    Profile stage stopped to solve solid problem

--- webapp/src/pages/profile/Profile.tsx
+++ webapp/src/pages/profile/Profile.tsx
@@ -1,11 +1,33 @@
 import "./profile.css";
+import Map from "../../components/map/Map";
+import UserRightbar from "../../components/rightbar/UserRightbar";
 function Profile(): JSX.Element {
 
   return (
-    <div className="profile">
-      <h1>Profile</h1>
-      <p>Hola!</p>
-    </div>
+    
+      <div className="profile">
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              
+              <img
+                className="profileUserImg"
+                src=
+                 "/noAvatar.png"
+                
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Andres</h4>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Map/>
+            <UserRightbar />
+          </div>
+        </div>
+      </div>
   );
 }
 
