646679ae0f1ef62936817a14e12b1e2e24ea9b7c
Diego <UO283615@uniovi.es>
2023-03-13 22:03:46 +0100

    Added a simple style to the login.

--- webapp/src/pages/login/Login.tsx
+++ webapp/src/pages/login/Login.tsx
@@ -3,6 +3,8 @@ import { LoginButton, CombinedDataProvider, useSession } from "@inrupt/solid-ui-
 import { Button, TextField, FormGroup, Container } from "@mui/material";
 import { makeRequest } from "../../axios";
 
+import "./login.css"
+
 const Login = () => {
     const [idp, setIdp] = useState("https://inrupt.net");
     const [currentUrl, setCurrentUrl] = useState("http://localhost:3000");
@@ -17,11 +19,11 @@ const Login = () => {
 
     return (
       <Container>
-        <h1> Login </h1>
-        <p> Welcome to LoMap!</p>
-        <p> This application runs using the solid principles, this means, you need an account on a pod provider to use it. </p>
-        <p> If you already have one, please log in.</p>
-        <p> If not, please create an account in a pod provider as inrupt.net</p>
+        <h1 className="loginHeader"> Login </h1>
+        <p className="loginText"> Welcome to LoMap!</p>
+        <p className="loginText"> This application runs using the solid principles, this means, you need an account on a pod provider to use it. </p>
+        <p className="loginText"> If you already have one, please log in.</p>
+        <p className="loginText"> If not, please create an account in a pod provider as inrupt.net</p>
         <FormGroup>
           <TextField
             label="Identity Provider"
@@ -41,7 +43,6 @@ const Login = () => {
             }}
           />
         </FormGroup>
-
       </Container>
     );
   }
