579610ab17fcefd3138dcadd3fb73f3734e9e0f8
Andrés Cadenas <andrescabla@gmail.com>
2023-03-14 11:12:23 +0100

    Fixed small problem

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -74,7 +74,7 @@ function App(): JSX.Element {
           element: <Profile />,
         },
         {
-          path: "/profile",
+          path: "/friends",
           element: <Friends />,
         },
         {
