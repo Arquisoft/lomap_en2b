632ebaf3afa086c2481cad15af1797dee4e43117
jjgancfer <jjgancfer@gmail.com>
2023-04-06 12:55:48 +0200

    Started adding the filter for users

--- webapp/src/pages/otherUsersLandmark/LandmarkFilter.tsx
+++ webapp/src/pages/otherUsersLandmark/LandmarkFilter.tsx
@@ -1,5 +1,21 @@
+import { Box, InputLabel, Select, MenuItem} from "@mui/material";
+import { User } from "../../shared/shareddtypes";
 
 
 export default function LandmarkFilter(props : any) : JSX.Element {
-    return null;
+
+    // TODO: Import here the users that are friends with the logged
+    const loadUsers = () => {
+        let userList : User[] = [];
+        return <Select id = "userFilter">
+            <MenuItem key = "all" value = "all">All users</MenuItem>
+            {userList.forEach(key => {
+                return <MenuItem value = {key}>{key}</MenuItem>
+            })}
+        </Select>
+    }
+    return <Box>
+         <InputLabel htmlFor="userFilter" name="userFilter" style={{color:"#FFF"}}>User</InputLabel>
+        {loadUsers}
+    </Box>;
 }
