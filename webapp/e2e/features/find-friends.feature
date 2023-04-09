8d21fe039a2398ab8524e19e82a4fefa5a20637c
plg22 <uo282763@uniovi.es>
2023-04-09 16:54:08 +0200

    Adding some base tests (Not complete)

--- webapp/e2e/features/find-friends.feature
+++ webapp/e2e/features/find-friends.feature
@@ -2,5 +2,10 @@ Feature: Finding people on the app
 
 Scenario: The user is logged in the site
   Given A logged user
-  When He searches for Pepe
+  When He searches for garabato
   Then Some test people should appear 
+
+Scenario: The user is logged in the site
+  Given A logged user
+  When He searches for asdfgh
+  Then No one should appear 
---
+++ webapp/e2e/steps/profile.steps.ts
@@ -0,0 +1,46 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import puppeteer from "puppeteer";
+
+const feature = loadFeature('./features/profile.feature');
+
+let page: puppeteer.Page;
+let browser: puppeteer.Browser;
+
+defineFeature(feature, test => {
+  
+  beforeAll(async () => {
+    browser = process.env.GITHUB_ACTIONS
+      ? await puppeteer.launch()
+      : await puppeteer.launch({ headless: false, slowMo: 50 });
+    page = await browser.newPage();
+
+    await page
+      .goto("http://localhost:3000", {
+        waitUntil: "networkidle0",
+      })
+      .catch(() => {});
+  });
+
+  test('The user is logged in the site', ({given,when,then}) => {
+    
+    let email:string;
+    let username:string;
+
+    given('A logged user', () => {
+      
+    });
+
+    when('I click on the profile', async () => {
+      await expect(page).toClick('button', { text: 'My Profile' })
+    });
+
+    then('I am able to see my information', async () => {
+      await expect(page).toMatch('My Profile')
+    });
+  })
+
+  afterAll(async ()=>{
+    browser.close()
+  })
+
+});
