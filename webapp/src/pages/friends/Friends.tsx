da54ca7b85fee59284c3bd09cdcc13b9724029e7
plg22 <uo282763@uniovi.es>
2023-04-10 21:43:13 +0200

    Added little css to friends and users

--- webapp/src/pages/friends/Friends.tsx
+++ webapp/src/pages/friends/Friends.tsx
@@ -1,6 +1,5 @@
-import { useSession } from '@inrupt/solid-ui-react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import "./friends.css";
+
 import { makeRequest } from '../../axios';
 import { useParams } from 'react-router';
 import { useQuery } from '@tanstack/react-query';
@@ -17,15 +16,6 @@ function Friends():JSX.Element {
             return res.data;
         })
       );
-
-  /*useEffect(() => {
-    const fetchFriends = async () => {
-      await makeRequest.get("/solid/"+uuid+"/friends").then((res) => {
-        setFriends(res.data);
-      });
-    };
-    fetchFriends();
-  },[friends,setFriends]); */
 
   return(
     <div className="friendsContainer">
