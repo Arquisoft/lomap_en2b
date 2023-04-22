af48cb1a6e9ac1aca1aaecb8d033f20f6d734570
jjgancfer <jjgancfer@gmail.com>
2023-04-22 12:48:08 +0200

    Renamed a folder, and created a new test file

---
+++ webapp/src/test/LandmarkFriend.test.tsx
@@ -0,0 +1,8 @@
+import {
+    screen,
+    render,
+    fireEvent
+} from "@testing-library/react";
+import LandmarkFriend from "../pages/otherUsersLandmark/LandmarkFriend"
+import {Landmark} from '../shared/shareddtypes';
+import assert from "assert";
