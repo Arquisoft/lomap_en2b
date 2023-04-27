20356ebd4902e490b3efffea1d790e6389bff721
plg22 <uo282763@uniovi.es>
2023-04-27 09:27:55 +0200

    Created up to 6 features for e2e tests (No login)

--- webapp/e2e/steps/add-landmark.steps.ts
+++ webapp/e2e/steps/add-landmark.steps.ts
@@ -1,7 +1,7 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import puppeteer from "puppeteer";
 
-const feature = loadFeature('../features/profile.feature');
+const feature = loadFeature('../features/add-landmarks.feature');
 
 let page: puppeteer.Page;
 let browser: puppeteer.Browser;
@@ -21,7 +21,7 @@ defineFeature(feature, test => {
       .catch(() => {});
   });
 
-  test('The user is logged in the site', ({given,when,then}) => {
+  test('The user logs in the site', ({given,when,then}) => {
     
     let email:string;
     let username:string;
@@ -30,12 +30,15 @@ defineFeature(feature, test => {
       
     });
 
-    when('I click on the friends tab', async () => {
-      await expect(page).toClick('Link', { text: 'Friends' })
+    when('I click on the addLandmark tab', async () => {
+      await expect(page).toClick('Link', { text: 'Add a landmark' })
     });
 
-    then('I am able to see my friends', async () => {
-      await expect(page).toMatch('Friends')
+    then('I am able to see my information', async () => {
+      await expect(page).toMatch('Name of the landmark')
+      await expect(page).toMatch('Category of the landmark')
+      await expect(page).toMatch('Latitude:')
+      await expect(page).toMatch('Longitude:')
     });
   })
 
