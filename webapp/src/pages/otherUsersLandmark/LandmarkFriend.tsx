53564672c004b64249ec651bdc643fd12fa58dda
Diego Villanueva <98838739+UO283615@users.noreply.github.com>
2023-05-01 14:55:41 +0200

    Change for solving conflicts

--- webapp/src/pages/otherUsersLandmark/LandmarkFriend.tsx
+++ webapp/src/pages/otherUsersLandmark/LandmarkFriend.tsx
@@ -17,7 +17,6 @@ import "../../map/stylesheets/addLandmark.css"
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {Landmark, LandmarkCategories, User} from "../../shared/shareddtypes";
 import L from "leaflet";
-import { getLocations } from "../../../solidHelper/solidLandmarkManagement";
 import { useSession } from "@inrupt/solid-ui-react";
 
 export default function LandmarkFriend() : JSX.Element{
